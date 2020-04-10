7628a538f808419dcc756d2d4ece1eba8b925abe
lorenuars19 <lorenuar.s19@gmail.com>
2020-04-10 15:08:52 +0200

    MAKEFILE POWAAR

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# wec-01
+# Morse code translator
+Simply overcomplicated morse code translator
+# Usage
+- `make`
+- `./morslator "morse code to translate"`
+- `./morslator -r "string to translate in morse code"`
---
+++ include/morslator.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   morslator.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/10 13:36:53 by lorenuar          #+#    #+#             */
+/*   Updated: 2020/04/10 15:01:16 by lorenuar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	_MORSLATOR_H
+# define _MORSLATOR_H
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+#endif
